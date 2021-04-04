bbe8f356b20ec500117823f5b58104cd6e9f2e24
Sajan Suthakaran <2478403S@student.gla.ac.uk>
2021-04-04 16:55:53 +0100

    Added two more tests corresponding to the profile view in tests.py

--- hashtagtube/tests.py
+++ hashtagtube/tests.py
@@ -10,7 +10,6 @@ class CategoryMethodTests(TestCase):
 		Example: "Random Category String" should be "random-category-string".
 		"""
 		category = add_category('Random Category String')
-		category.save()
 
 		self.assertEqual(category.slug, 'random-category-string')
 
@@ -38,8 +37,33 @@ class IndexViewTests(TestCase):
 		num_categories = len(response.context['categories'])
 		self.assertEquals(num_categories, 3)
 
+class ProfileViewTests(TestCase):
+	def test_profile_view_with_no_categories(self):
+		response = self.client.get(reverse('hashtagtube:profile'))
+
+		self.assertEqual(response.status_code, 200)
+		self.assertQuerysetEqual(response.context['categories'], [])
+	
+	def test_profile_view_with_categories(self):
+		"""
+		Checks whether categories are displayed correctly when present.
+		"""
+		add_category('Music')
+		add_category('Sport')
+		add_category('Food')
+		
+		response  = self.client.get(reverse('hashtagtube:profile'))
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Music")
+		self.assertContains(response, "Sport")
+		self.assertContains(response, "Food")	
+
+		num_categories = len(response.context['categories'])
+		self.assertEquals(num_categories, 3)		
+
 def add_category(title):
 	category = Category.objects.get_or_create(title=title)[0]
 	
 	category.save()
 	return category
+
