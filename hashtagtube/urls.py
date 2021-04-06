a6d16bcbe221b0fc7b64c58ef54f658552fbdfa6
Yanzhi Shu <2411021s@student.gla.ac.uk>
2021-04-06 20:37:07 +0800

    Upload video

--- hashtagtube/urls.py
+++ hashtagtube/urls.py
@@ -5,15 +5,15 @@ app_name = 'hashtagtube'
 
 urlpatterns = [
     path('', views.index, name='index'),
-   #path('login/', views.user_login, name='login'),
-   #path('register/',views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('register/',views.register, name='register'),
     path('category/<slug:category_name_slug>/',
     	 views.show_category, name='show_category'),
     path('add_category/',views.add_category, name='add_category'),
     path('category/<slug:category_name_slug>/add_video/',
     	  views.add_video, name='add_video'),
     path('restricted/',views.restricted, name='restricted'),
-   #path('logout/',views.user_logout, name='logout'),
+    path('logout/',views.user_logout, name='logout'),
     path('like_video/', views.like, name='like_video'),
     path('dislike_video/', views.dislike, name='dislike_video'),
     path('love_video/', views.love, name='love_video'),
