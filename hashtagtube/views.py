d44fce14c936dc9a731f95ad78d030738251c2f7
Sajan Suthakaran <2478403S@student.gla.ac.uk>
2021-04-06 01:01:17 +0100

    Commented out unnecessary code in views.py and urls.py, and also modified base.html

--- hashtagtube/views.py
+++ hashtagtube/views.py
@@ -135,97 +135,97 @@ def restricted(request):
     return render(request, 'hashtagtube/restricted.html', context=context_dict)
 
 
-def user_login(request):
+#def user_login(request):
     # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+   # if request.method == 'POST':
+       # username = request.POST.get('username')
+       # password = request.POST.get('password')
 
         # Use Django's machinery to attempt to see if the username/password
         # combination is valid - a User object is returned if it is
-        user = authenticate(username=username, password=password)
+       # user = authenticate(username=username, password=password)
 
         # If we have a User object, the details are correct.
-        if user:
+        #if user:
             # Is the account is active? It could have been disabled
-            if user.is_active:
+           # if user.is_active:
                 # If the account is valid and active, we can log the user in.
                 # We'll send the user back to the homepage.
-                lgoin(request, user)
-                return redirect(reverse('hashtagtube:index'))
-            else:
+              #  lgoin(request, user)
+               # return redirect(reverse('hashtagtube:index'))
+           # else:
                 # An inactive account was used - no logging in!
-                return HttpResponse("Your hashtagtube account is disabled.")
-        else:
+            #    return HttpResponse("Your hashtagtube account is disabled.")
+      #  else:
             # Bad login details was provided, so we can't log the user in.
-            print("Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid lgoin details supplied.")
+       #     print("Invalid login details: {username}, {password}")
+        #    return HttpResponse("Invalid lgoin details supplied.")
     # The request is not a HTTP POST, so display the login form
-    else:
-        return render(request, 'registration/login.html')
+   # else:
+     #   return render(request, 'registration/login.html')
 
 
 @login_required
-def user_logout(request):
-    logout(request)
+#def user_logout(request):
+ #   logout(request)
 
-    return render(request, ('registration/logout.html'))
+  #  return render(request, ('registration/logout.html'))
 
 
-def register(request):
+#def register(request):
     # A boolean value for telling the template
     # whether the registration was successful.
     # Set to False initially.
     # Code changes value to True when registration succeeds.
-    registered = False
+ #   registered = False
 
     # If it's a HTTP POST
-    if request.method == 'POST':
+  #  if request.method == 'POST':
         # Attempt to grab information from the raw form information.
         # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
+   #     user_form = UserForm(request.POST)
+    #    profile_form = UserProfileForm(request.POST)
 
         # If the two forms are valid...
-        if user_form.is_valid() and profile_form.is_valid():
+     #   if user_form.is_valid() and profile_form.is_valid():
             # Save the user's form date to the database.
-            user = user_form.save()
+      #      user = user_form.save()
 
             # Now we hash the password with the set_password method.
             # Once hashed, we can update the user object.
-            user.set_password(user.password)
-            user.save()
+       #     user.set_password(user.password)
+        #    user.save()
 
             # Now sort out the UserProfile instance.
             # Since we need to set the user attribute ourselves.
             # we set commit=False. This delays saving the model
             # until we're ready to avoid integrity problems.
-            profile = profile_form.save(commit=False)
-            profile.user = user
+         #   profile = profile_form.save(commit=False)
+          #  profile.user = user
 
             # Did the user provdie a profile picture?
             # If so, we need to get it from the input form and
             # put it in the UserProfile model.
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
+          #  if 'picture' in request.FILES:
+           #     profile.picture = request.FILES['picture']
 
-            profile.save()
+           # profile.save()
 
-            registered = True
-        else:
+           # registered = True
+       # else:
             # Invalid form or forms
             # Print problems to the terminal
-            print(user_form.errors, profile_form.errors)
-    else:
+        #    print(user_form.errors, profile_form.errors)
+   # else:
         # Not a HTTP POST, so we render our form using two ModelForm instances.
         # These forms will be blank, ready for user input.
-        user_form = UserForm()
-        profile_form = UserProfileForm()
+    #    user_form = UserForm()
+     #   profile_form = UserProfileForm()
     # render the template depending on the context.
-    return render(request, 'hashtagtube.register.html',
-                  context={'user_form': user_form,
-                           'profile_form': profile_form,
-                           'registered': registered})
+   # return render(request, 'hashtagtube.register.html',
+    #              context={'user_form': user_form,
+     #                      'profile_form': profile_form,
+      #                     'registered': registered})
 
 
 @login_required
