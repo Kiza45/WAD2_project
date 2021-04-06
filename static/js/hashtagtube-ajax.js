a2d7fd9d479d975a2c674aa4caee8cc8fc97ba09
“Jan <acaisrai@gmail.com>
2021-04-06 10:57:39 +0100

    done the ajax part for following/unfollowing another users

--- static/js/hashtagtube-ajax.js
+++ static/js/hashtagtube-ajax.js
@@ -1,73 +1,102 @@
 $(document).ready(
+  //function updating the like count for the video on button click
     $('#like_btn').click(function(){
+    //get id of the videopage
     var pageID;
     pageID = $(this).attr('data-pageid');
 
+    //call apropriate view function and update the like count
     $.get('/hashtagtube/like_video/',
           {'page_id': pageID},
           function(data){
               $('#like_count').html(data);
-            //  $(buttonID).hide(); //dunno if we should hide the button
           })
     });
 
+//function updating the dislike count for the video on button click
     $('#dislike_btn').click(function(){
+    //get id of the videopage
     var pageID;
     pageID = $(this).attr('data-pageid');
 
+    //call apropriate view function and update the dislike count
     $.get('/hashtagtube/dislike_video/',
           {'page_id': pageID},
           function(data){
               $('#dislike_count').html(data);
-            //  $(buttonID).hide(); //dunno if we should hide the button
           })
     });
 
+//function updating the love count for the video on button click
     $('#love_btn').click(function(){
+    //get id of the videopage
     var pageID;
     pageID = $(this).attr('data-pageid');
 
+    //call apropriate view function and update the love count
     $.get('/hashtagtube/love_video/',
           {'page_id': pageID},
           function(data){
               $('#love_count').html(data);
-            //  $(buttonID).hide(); //dunno if we should hide the button
           })
     });
 
+//function updating the haha count for the video on button click
     $('#haha_btn').click(function(){
+    //get id of the videopage
     var pageID;
     pageID = $(this).attr('data-pageid');
 
+    //call apropriate view function and update the haha count
     $.get('/hashtagtube/haha_video/',
           {'page_id': pageID},
           function(data){
               $('#haha_count').html(data);
-            //  $(buttonID).hide(); //dunno if we should hide the button
+
           })
     });
 
+//submit the comment to the video without a page reloading
     $('#submit_comment_btn').click(function(){
       //get the id of a videopage
       var pageID;
       pageID = $(this).attr('data-pageid');
 
-      //call the appropriate view
+      //call the appropriate view and inform the user comment has been added
       $.get('/hashtagtube/submit_comment/',
             {'page_id': pageID},
-            //append a new comment to the comments element of the html(is it the right way)
             function(data){
-              $('#comments').append(data);
+              alert('Comment successfully added.');
             })
     })
 
-//need to correct when templates done
-//    $('#follow').click(function(){
-//    $.get('/hashtagtube/haha_video/',
-//          {'page_id': pageID},
-//          function(data){
-//              $('#haha_count').html(data);
-            //  $(buttonID).hide(); //dunno if we should hide the button
-//          })
-//    });
+//letting user follow another user profile
+    $('#follow').click(function(){
+      //obtain id of the user profile a user wants to follow
+        var userID;
+        userID = $(this).attr('data-userprofileid');
+
+        //call the appropriate view and inform the user they have successfully followed
+        //another user
+        $.get('/hashtagtube/follow_unfollow/',
+              {'user_profile_id': userID},
+              function(data){
+                alert('You successfully followed this account.');
+              })
+    })
+
+//letting user unfollow another user profile
+    $('#unfollow').click(function(){
+      //obtain id of the user profile a user wants to follow
+        var userID;
+        userID = $(this).attr('data-userprofileid');
+
+        //call the appropriate view and inform the user they have successfully followed
+        //another user
+        $.get('/hashtagtube/follow_unfollow/',
+              {'user_profile_id': userID},
+              function(data){
+                alert('You successfully unfollowed this account.');
+              })
+    })
 );
--- static/js/hashtagtube-jquery.js
+++ static/js/hashtagtube-jquery.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
 //when user clicks follow button-change colour and id,
-//now user can unfollow this other user
+//once followed, user can then unfollow aanother user
     $('#follow').click(function(){
         $(this).css('color', 'grey');
+        //when unfollowing, change button's id and text to unfollow-next available state
         $(this).removeAttr('id').attr('id', 'unfollow');
         $(this).text('Unfollow');
     });
-    //adjust colors when know which colors used
+
+//if user follows another user, the only thing they can do is unfollow them
+//hence id and text unfollow
     $('#unfollow').click(function(){
         $(this).css('color', 'red');
+        //when unfollowing, change button's id and text to follow-next available state
         $(this).removeAttr('id').attr('id', 'follow');
-        $(this).text('Unfollow');
+        $(this).text('Follow');
     });
 
 //when users clicks a react button, it appears as clicked
@@ -19,8 +23,10 @@ $(document).ready(function() {
       //obtain rection's id to manipulate the right button
         var react_type = '#'+$(this).attr('id');
         $(react_type).toggle(function(){
+          //when reaction is clicked, batton changed to a shadow/darker background
             $(this).css('background', 'grey');
         }, function(){
+          //when we clicked a clicked reaction, button is changed to original(no background) state
             $(this).css('background', 'none');
         });
     });
